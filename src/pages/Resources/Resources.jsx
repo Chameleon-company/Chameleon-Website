8272df4bc161c18c68df0da5f7b77ad93ef721b4
Jack <jackhagen9@gmail.com>
2022-09-18 23:27:14 +1000

    Calendar component

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Heading from '../../components/heading/Heading';
+import Calendar from './components/Calendar';
+import './Resources.css';
 
 function Resources() {
   return (
-    <div>Resources</div>
+<div>
+<Heading name ="Resources Centre"/>
+<div class="grid-container">
+
+<div class="grid-child purple">
+    <Calendar/>
+</div>
+
+<div class="grid-child green">
+{/* FAQ Drop Down Menu goes here */}
+</div>
+
+</div>
+</div>
+
   )
 }
 

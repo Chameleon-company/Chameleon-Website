449bb7d271707d46ed54ed95ddbf7d78b6e2ee4c
JulianRyan <81020315+JulianRyan@users.noreply.github.com>
2022-09-19 20:33:14 +1000

    initial

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Heading from '../../components/heading/Heading'
-
+import CompanyInfoSection from './components/CompanyInfoSection'
+import './components/CompanyInfoSection.css'
 
 function AboutUs() {
   return (
-    <Heading name = "About Us"/>
+    <main>
+      <Heading name = "About Us"/>
+      <CompanyInfoSection />
+    </main>
   )
 }
 

e551b40afbe4ef399c5aed1f47a9fa78d50f2c89
Nykolai-McHerron <nmcherro@deakin.edu.au>
2022-09-19 21:25:13 +1000

    Merging progressBar component - Nykolai

--- src/pages/OurServices/OurServices.jsx
+++ src/pages/OurServices/OurServices.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import Card from '../../components/card/Card';
-import Contacts from '../../data/servicesData';
+
+//imported but not used
+//import Card from '../../components/card/Card';
+//import Contacts from '../../data/servicesData';
+
+//Importing progress bar function component & CSS Stylesheet
+import ProgressBar from './ourServicesComponents/progressBar_component';
+import './ourServicesComponents/progressBar_styling.css'
 
 function createCard(contact){
   
@@ -8,8 +14,22 @@ function createCard(contact){
 
 function OurServices() {
   return (
-    <div>OurServices</div>
+    <div>
+      <h1>Our Services</h1>
+
+      {/* applying page alignment css to all progress bars within the div */}
+      <div className='progressBar_pageAlignment'>
+
+      {/* calling progressBar function & updating values percent and title values */}
+      <ProgressBar progressPercent={84} progressTitle="Deadlines Met"/>
+      <ProgressBar progressPercent={90} progressTitle="Within Budget"/>
+      <ProgressBar progressPercent={96} progressTitle="Client Satisfaction"/>
+      <ProgressBar progressPercent={100} progressTitle="Carbon Neutral"/>     
+      </div>
+
+    </div>
   )
 }
+
 
 export default OurServices

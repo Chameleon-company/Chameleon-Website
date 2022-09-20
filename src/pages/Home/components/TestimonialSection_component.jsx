2c9812aa34f215551a02c86559c7dff6646da0c1
Zhenyu11 <106434342+Zhenyu11@users.noreply.github.com>
2022-09-20 15:20:44 +1000

    Testimonial

---
+++ src/pages/Home/components/TestimonialSection_component.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import './TestimonialSection_styling.css'
+import '../../../index.css' //website template
+
+function TestimonialSection_Heading_function() {
+    return (
+        <div className='TestimonialSection'>
+        <div className="head">
+            <div>Testimonials</div>
+        </div>
+        </div>
+    );
+}
+
+function TestimonialSection_Content_function() {
+
+    const leftquotessign1 = require("@/assets/images/left-quotes-sign 1.png");
+
+    return(
+        <div className='TestimonialSection'>
+        <div className="main">
+            <div class="li">
+                <div class="top">
+                    <div class="text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut
+                    </div>
+                    <div class="img">
+                    <img src="leftquotessign1" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="li liRight">
+                <div class="top">
+                    <div class="text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut
+                    </div>
+                    <div class="imgRight">
+                    <img src="leftquotessign1" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+function TestimonialSection_User_function() {
+
+    const Ellipse1 = require("@/assets/images/Ellipse1.png");
+    const Ellipse2 = require("@/assets/images/Ellipse2.png");
+
+    return (
+        <div className='TestimonialSection'>
+        <div className='user'>
+            <div class="li">
+                <div class="bottom">
+                    <div>
+                    <img src="Ellipse1" alt="" />
+                    </div>
+                    <div>Robert Smith</div>
+                </div>
+            </div>
+            <div class="li liRight">
+                <div class="bottom">
+                    <div>
+                    <img src="Ellipse2" alt="" />
+                    </div>
+                    <div>Jane Fibbs</div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}

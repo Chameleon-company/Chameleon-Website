e7d493e7a5bb59bf67c096cb76b1a7c0a2208d03
unknown <shlimon@deakin.edu.au>
2022-09-22 14:08:44 +1000

    fixing import name

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Heading from '../../components/heading/Heading'
 import CompanyInfoSection from './components/CompanyInfoSection'
-import CompanyInfoSection from './components/CompanyInfoSection'
+import StatsSection from './components/StatsSection'
 import './components/CompanyInfoSection.css'
 
 function AboutUs() {

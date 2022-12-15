a55fbe6c979c4c7910c9b56cf482fca5dd97df38
Jason Hu <sq.tiyu@gmail.com>
2022-12-15 18:01:09 +1100

    mailchimp newsletter

--- src/pages/Home/components/HeroSection.jsx
+++ src/pages/Home/components/HeroSection.jsx
@@ -7,9 +7,12 @@ function HeroSection() {
     <div className="heroImage">
         <div class="heroText">
         <h1 className="heroHeading">Welcome to Chameleon</h1>
-        <p className = "heroPara">Striving to Create a Smarter World</p>
-        <Button btnName = "Subscribe"/>
-  </div>
+        <p className = "heroPara">Striving to Create a Smarter World</p>  
+        <a href="https://mailchi.mp/3d807f37a793/chameleon-newsletter">
+          <Button btnName = "Subscribe"></Button>
+        </a>
+
+  </div>  
     </div>
   )
 }

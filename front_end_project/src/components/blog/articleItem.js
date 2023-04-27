eee916db0eb4a6ffe6910bbfc37f97b163ab06ea
Rtam22 <regantam92@gmail.com>
2023-04-28 01:49:04 +1000

    Changed styles to match design

--- front_end_project/src/components/blog/articleItem.js
+++ front_end_project/src/components/blog/articleItem.js
@@ -9,7 +9,9 @@ const ArticleItem = (props) => {
         props.clickHandler(props.article, props.title);
       }}
     >
-      <img className="item-image" src={props.image}></img>
+      <div className="item-image-container">
+        <img className="item-image" src={props.image}></img>
+      </div>
       <p className="item-title">{props.title}</p>
       <hr className="item-separator"></hr>
     </div>
--- front_end_project/src/components/blog/articles/articlelist.js
+++ front_end_project/src/components/blog/articles/articlelist.js
@@ -4,19 +4,22 @@ const articleList = [
   {
     id: 0,
     title: 'Why should you buy an EV',
-    image: 'https://via.placeholder.com/600x400',
+    image:
+      'https://images.unsplash.com/photo-1672152774636-54492a200f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=969&q=80',
     article: Article.WhyBuyEV,
   },
   {
     id: 1,
     title: 'What is a smart city',
-    image: 'https://via.placeholder.com/600x400',
+    image:
+      'https://images.unsplash.com/photo-1494522855154-9297ac14b55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     article: Article.smartCity,
   },
   {
     id: 2,
     title: 'Article 3',
-    image: 'https://via.placeholder.com/600x400',
+    image:
+      'https://images.unsplash.com/photo-1525935944571-4e99237764c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
     article: Article.whyBuyEv2,
   },
 ];

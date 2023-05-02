e11728b6b0b84202b4bb81b3b29b9170c0707fa2
Mayankv20 <62798875+Mayankv20@users.noreply.github.com>
2023-05-02 23:17:47 +1000

    created a portfolio page

--- front_end_project/src/pages/home/home.js
+++ front_end_project/src/pages/home/home.js
@@ -1,17 +1,31 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import React, { Component } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 class Home extends Component {
   render() {
     return (
-       
-        
-        <Container>
+      <Container>
         <Row>
           <h1>Home</h1>
           <Col xs={12}>
             <div>
-             
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
           </Col>
         </Row>

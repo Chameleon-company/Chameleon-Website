d63d24b23a1d77138041e43efa9cec7a7613b25b
Ziyan Zhai(Tom) <102450766+zhai1027@users.noreply.github.com>
2023-05-17 21:52:28 +1000

    Homepage (#36)
    * Homepage (front-end)
    
    By Ziyan Zhai
    
    * Delete Header.js
    
    deleting header js as this has already been done and working
    
    * Delete Header.css
    
    * +Origin header design
    
    Add header into the homepage page.
    
    ---------
    
    Co-authored-by: janviig <83744327+janviig@users.noreply.github.com>

--- front_end_project/src/pages/homepage/index.js
+++ front_end_project/src/pages/homepage/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import Header from './Header';
+import Header from './Header';
+
 import Welcome from './Welcome'
 import Video from './Video'
 import Comment from './Comments'
 import BottomBar from './BottomBar';
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <div>
+//     <Header /> 
+
+//     <Welcome />
+    
+//     <Video />
+
+//     <Comment />
+
+//     <BottomBar />
+
+//   </div>
+// );
 
 function Index() {
 
@@ -28,11 +47,3 @@ function Index() {
 
 
 export default Index;
-
-
-
-/*
-Introduction in Welcome node, reference: https://www.g2.com/products/chameleon/reviews
-  "Chameleon is a sophisticated no-code platform for product success..."
-  This is referencing by the website above.
-*/

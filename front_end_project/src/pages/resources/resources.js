6c8b4aed2713cbf926dce76087065bbd16eeca3b
Aaron Vandersluys <87591425+Aaron-Vandersluys-Code@users.noreply.github.com>
2023-08-07 12:52:01 +1000

    Resources page and navigation bar updates
    -Updated navigation bar to include What Is IoT section when hovering over resources tab.
    
    -Emerging tech page has been updated to become What Is IoT page. This is to reflect identified updates to resources section discovered in sprint.
    
    -Added new heading and subheading to What Is IoT? page
    
    -Updated info rectangles to include two new sections, Internet of Things Technologies and IoT Statistics in Australia. These new sections will contain a brief introduction to these concepts, and link to more detailed information pages, which are yet to be added. Placeholder links are in place.
    
    -Updated names of divs to be more reflective of their content.

--- front_end_project/src/pages/resources/resources.js
+++ front_end_project/src/pages/resources/resources.js
@@ -1,99 +1,98 @@
-import { Container, Row, Col } from "react-bootstrap";
-import React, { Component } from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import React, { Component } from 'react';
 import Rectangle from 'react-rectangle';
 import './resources.css';
-import smartCity from './images/city.png'
-import tech from './images/tech.png'
+import smartCity from './images/city.png';
+import tech from './images/tech.png';
 
 class Resources extends Component {
   render() {
     return (
       <Container>
         <Row>
-          <div className="top">
-            Emerging Tech
+          <div className="pageHeading">What Is IoT Technology?</div>
+
+          <div className="pageSubHeading">
+            <p>
+              <p>An Introduction To Internet Of Things</p>
+            </p>
           </div>
+
           {/* <Col xs={12}> */}
-            <div className="rectangle">
-              <Rectangle className="first">
-                <div style={{ background: '#E4F0FF' }} />
+          <div className="rectangle">
+            <Rectangle className="iotRec">
+              <div style={{ background: '#E4F0FF' }} />
 
-                {/* image */}
-                <div className="smart">
-                  <img src={smartCity} width="300" height="250"></img>
-                </div>
-                {/* text */}
+              {/* image */}
+              <div className="iotImage">
+                <img
+                  src={smartCity}
+                  width="300"
+                  height="250"
+                  alt="City view"
+                ></img>
+              </div>
+              {/* text */}
 
-                <div className="heading">
-                  <h3>What is a smart city?</h3>
-                </div>
+              <div className="iotHeading">
+                <h3>Internet of Things Technologies</h3>
+              </div>
 
-                <div className="text4">
-                  <p>
-                    A smart city uses technology to connect,
-                    protect and enhance the lives of citizens
-                    using IoT sensors, video cameras and other
-                    inputs. The aim is to provide the city operator
-                    and citizens with feedback so they can make
-                    informed decisions.
-                  </p>
-                </div>
+              <div className="iotLearnMore">
+                <a href="">Click here to learn more about IoT</a>
+              </div>
 
-                <div className="textsmart">
-                  <p>
-                    A smart city collects and analyse data from
-                    sensors and cameras and send to the city
-                    operator so the action can be taken, sometimes
-                    automatically. For example, a public bin can
-                    alert the city service for a collection.
-                  </p>
-                </div>
+              <div className="iotPara">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Pellentesque odio diam, molestie eu purus vel, consectetur
+                  dapibus turpis. Fusce non sapien at ipsum scelerisque
+                  consequat. Ut vitae quam ultricies, mattis massa tempus,
+                  malesuada ligula. Pellentesque laoreet.
+                </p>
+              </div>
+            </Rectangle>
+          </div>
 
-              </Rectangle>
-            </div>
-       
-            <br></br>
+          <div className="rectangle2">
+            <Rectangle className="statsRec">
+              <div style={{ background: '#E4F0FF' }} />
 
-            <div className="rectangle25">
-              <Rectangle className="secondrec">
-                <div style={{ background: '#E4F0FF' }} />
+              {/* image */}
+              <div className="statsImage">
+                <img
+                  src={tech}
+                  width="300"
+                  height="250"
+                  alt="Computer components digital"
+                ></img>
+              </div>
+              {/* text */}
 
-                {/* image */}
-                <div className="tech">
-                  <img src={tech} width="300" height="190"></img>
-                </div>
-                {/* text */}
+              <div className="statsHeading">
+                <h3>IoT Statistics in Australia</h3>
+              </div>
 
-                <div className="heading2">
-                  <h3>Emerging Technologies in 2023</h3>
-                </div>
+              <div className="statsLearnMore">
+                <a href="">
+                  Click here to learn more about IoT Stats in Australia
+                </a>
+              </div>
 
-                <div className="techText">
-                  <p>
-                    Emerging technologies are the technologies
-                    that have not yet been commercialised but
-                    show great potential to change our world.
-                    It is considered to be the trend and in
-                    their early stages.
-                  </p>
-                </div>
-
-                <div className="techText2">
-                  <p>
-                    Emerging technologies in 2023 are:
-                  </p>
-                  <p> 1. Neuromorphic computing</p>
-                  <p> 2. Self-supervised learning</p>
-                  <p>3. Human-centred AI</p>
-                </div>
-              </Rectangle>
-            </div>
-            <div>
-            </div>
+              <div className="statsPara">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Pellentesque odio diam, molestie eu purus vel, consectetur
+                  dapibus turpis. Fusce non sapien at ipsum scelerisque
+                  consequat. Ut vitae quam ultricies, mattis massa tempus,
+                  malesuada ligula. Pellentesque laoreet.
+                </p>
+              </div>
+            </Rectangle>
+          </div>
+          <div></div>
           {/* </Col> */}
         </Row>
-
-
       </Container>
     );
   }

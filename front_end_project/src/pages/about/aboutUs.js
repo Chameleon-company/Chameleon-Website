49f2dadb414b13ea58eeeda9fa8ab56350bc7c14
Davinder-2001 <83646254+Davinder-2001@users.noreply.github.com>
2023-09-09 21:05:29 +1000

    About Us Page Content Upload
    **Pull Request: Update About Us Page Content**
    
    This pull request includes updates to the content for the About Us page, specifically:
    
    1. **Company Overview**: We've revised and improved the company overview section to provide a clearer and more engaging description of our organization.
    
    2. **FAQs**: We've added detailed answers to frequently asked questions (FAQs) to ensure visitors to the About Us page can find the information they need easily.
    
    These updates enhance the About Us page and improve the overall user experience. Please review and merge at your earliest convenience.
    
    Thank you!

--- front_end_project/src/pages/about/aboutUs.js
+++ front_end_project/src/pages/about/aboutUs.js
@@ -50,7 +50,7 @@ class aboutUs extends Component {
               ></iframe>
             </Col>
             <Col>
-              <h2>Chameleon is a Deakin Universty, student-led company....</h2>
+              <h2>Chameleon is a Deakin Universty, student-led company dedicated to revolutionizing urban living through the strategic application of IoT-based solutions. Our organization is structured into three distinct divisions, each focused on key areas of strategic importance.</h2>
             </Col>
           </Row>
         </Container>
@@ -148,21 +148,32 @@ class aboutUs extends Component {
               <Accordion.Header>
                 <h3>What does Chameleon do?</h3>
               </Accordion.Header>
-              <Accordion.Body>Content #1</Accordion.Body>
+              <Accordion.Body>Chameleon is an organization dedicated to creating innovative IoT-based solutions to enhance urban life. They focus on various divisions, including City of Melbourne Open Data, Chameleon Website, and Electric Vehicle (EV) Adoption Tools. These divisions work together to address urban challenges, promote sustainability, provide user-friendly access to project information, and champion the adoption of electric vehicles for a greener future. 
+Chameleon's overarching mission is to make cities smarter, more livable, and environmentally friendly through their strategic approach and collaborative efforts.
+</Accordion.Body>
             </Accordion.Item>
             <br></br>
             <Accordion.Item eventKey="1">
               <Accordion.Header>
                 <h3>How can I sign up?</h3>
               </Accordion.Header>
-              <Accordion.Body>Content #2</Accordion.Body>
+              <Accordion.Body>You can easily sign up with us by following these steps:
+                <ul>
+                <li><strong>Visit Our Website:</strong> Start by visiting our website, where you'll find a user-friendly sign-up or registration page.</li>
+                <li><strong>Create Your Account:</strong>  Click on the "Sign Up" or "Register" button and provide the required information. This includes your name, email address, and a password.</li>
+                <li><strong>Verify Your Email:</strong>  After you've filled in the necessary details, you will receive a verification email. Click the link in the email to confirm your registration.</li>
+                <li><strong>Complete Your Profile:</strong>  Log in to your newly created account and complete your profile by adding any additional information or preferences.</li>
+                <li><strong>Start Exploring:</strong>  Once your profile is set up, you can start exploring our platform, accessing resources, and engaging with our projects.</li>
+                </ul>
+If you need assistance, feel free to reach out to our support team, and they will be happy to guide you through the process. You can find them under the support page or you can contact them using the phone number or email address provided at the bottom of each page on our website. We're excited to have you on board!
+</Accordion.Body>
             </Accordion.Item>
             <br></br>
             <Accordion.Item eventKey="2">
               <Accordion.Header>
                 <h3>Where can I find a progress update?</h3>
               </Accordion.Header>
-              <Accordion.Body>Content #2</Accordion.Body>
+              <Accordion.Body>You can conveniently access progress updates for our projects through our Chameleon Website (CW). Our website serves as a central hub for comprehensive information about all our projects, including their objectives and ongoing developments. Simply navigate to the "Projects" or "News" section on our website to find the latest updates and milestones achieved by our City of Melbourne Open Data (MOP), Chameleon Website (CW), and Electric Vehicle (EV) Adoption Tools (EVAT) divisions. We regularly update this section to keep you informed about our progress in making cities smarter, more livable, and environmentally friendly.</Accordion.Body>
             </Accordion.Item>
           </Accordion>
           <br></br>

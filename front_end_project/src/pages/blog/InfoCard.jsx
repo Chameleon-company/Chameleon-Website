98638f155fcb703d8cfa11bda25ca49f285fce83
Bhavikasood88 <119603676+Bhavikasood88@users.noreply.github.com>
2023-09-13 20:57:12 +0500

    News page
    News page update

--- front_end_project/src/pages/blog/InfoCard.jsx
+++ front_end_project/src/pages/blog/InfoCard.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const InfoCard = ({ img, title, shortInfo, date }) => {
   return (
     <div className='w-full  flex flex-col md:justify-start justify-center md:items-start items-center'>
+<<<<<<< Updated upstream
       <div className=' w-[17rem] md:w-[22rem] h-[15rem]'>
+=======
+      <div className='w-[22rem] h-[15rem]'>
+>>>>>>> Stashed changes
         <img src={img} alt='' className='w-full h-full object-cover' />
       </div>
       <h1 className='uppercase text-2xl mt-3'>{title}</h1>
--- front_end_project/src/pages/blog/blog.js
+++ front_end_project/src/pages/blog/blog.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Wrapper from '../../components/shared/Wrapper';
 import InfoCard from './InfoCard';
 import BlogJSON from '../../constants/Blog.json';
+<<<<<<< Updated upstream
 import Footer from '../../components/blog/Footer';
+=======
+>>>>>>> Stashed changes
 const Blog = () => {
   return (
     <Wrapper id='#blog' Style='w-full h-[100vh-80px]'>
@@ -31,15 +34,23 @@ const Blog = () => {
 
       <div className='w-full my-4 flex flex-col gap-4 md:gap-0 md:flex-row justify-between items-center bg-green-emerald px-16 py-3'>
         <h1 className='uppercase text-white'>get updates</h1>
+<<<<<<< Updated upstream
         <div className='h-full flex items-center '>
+=======
+        <div className='h-full flex items-center ' >
+>>>>>>> Stashed changes
           <input
             type='text'
             placeholder='Your email address here'
             className=' md:w-[20rem] h-full rounded-sm py-3 pl-2 text-black'
           />
+<<<<<<< Updated upstream
           <button className='bg-[#054a83] px-4 rounded-sm text-white h-full py-3'>
             Subcribe
           </button>
+=======
+          <button className='bg-[#054a83] px-4 rounded-sm text-white h-full py-3'>Subcribe</button>
+>>>>>>> Stashed changes
         </div>
       </div>
 
@@ -65,7 +76,68 @@ const Blog = () => {
         {/* Pagination(Sroll to ability here) */}
       </div>
 
+<<<<<<< Updated upstream
       <Footer />
+=======
+      <div className="w-full h-full bg-blue py-2 px-4">
+        <div className="grid md:grid-cols-2 ">
+          <div className="w-full h-full flex items-center">
+            <div className="w-[100px] h-[100px]">
+              <img
+                src="/AboutUs/logo.png"
+                alt=""
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <div className="w-full h-full flex flex-col justify-start items-start">
+              <div className="flex  md:justify-start justify-center">
+                <img
+                  src="/AboutUs/insta.png"
+                  alt=""
+                  className="w-[40px] h-[40px] object-contain rounded-md"
+                />
+                <img
+                  src="/AboutUs/facebook.png"
+                  alt=""
+                  className="w-[40px] h-[40px] object-contain rounded-md"
+                />
+                <img
+                  src="/AboutUs/twitter.png"
+                  alt=""
+                  className="w-[40px] h-[40px] object-contain rounded-md"
+                />
+                <img
+                  src="/AboutUs/Mail.png"
+                  alt=""
+                  className="w-[40px] h-[40px] object-contain rounded-md"
+                />
+              </div>
+              <div>
+                <h1 className=" text-2xl text-white flex md:justify-start justify-center">
+                  &copy;2023 Copyright reserved by Chameleon
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-full flex justify-between items-center">
+            <div className="w-full h-full pt-3 flex flex-col justify-start items-start font-semibold text-white">
+              <div>
+                <h1 className="text-xl text-center mb-2">Contact Us</h1>
+                <p className="text-lg text-center">PH: 03 59756586</p>
+                <p className="text-lg text-center">E:info@chameleon.com</p>
+              </div>
+            </div>
+            <div className="w-[100px] h-[100px]">
+              <img
+                src="/AboutUs/deakin.png"
+                alt=""
+                className="w-full h-full object-contain"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </Wrapper>
   );
 };

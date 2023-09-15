5709c4ce17ec92904d1a8678f655175a0d55f72a
Davinder-2001 <83646254+Davinder-2001@users.noreply.github.com>
2023-09-15 15:07:26 +1000

    Added Footer Component
    GLOBAL CHANGES:
    Footer is now added and is visible across all the pages on the Chameleon website.
    
    LOCAL CHANGES:
    Formatting has been improved on the about us page; includes the max-width for the containers and font-size.

--- front_end_project/src/App.js
+++ front_end_project/src/App.js
@@ -1,5 +1,4 @@
 import Header from "./layouts/header/header";
-import Footer from "./layouts/footer/footer";
 import HomePage from "./pages/homepage/Homepage";
 import Helloo from "./pages/hello/hello";
 import aboutUs from "./pages/about/aboutUs";
@@ -16,37 +15,38 @@ import Report from "./pages/report/report";
 import forgot from "./pages/forgot_password/forgot";
 import Login from "./pages/login/login";
 import Signup from "./pages/signup/signup";
+import Footer from "./layouts/footer/footer";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-        <Router>
-          <Header />
-          <main>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/home" component={HomePage} />
-              <Route path="/hello" component={Helloo} />
-              <Route path="/about_us" component={aboutUs} />
-              <Route path="/blog" component={Blog} />
-              <Route path="/news" component={News} />
-              <Route path="/projects" component={Projects} />
-              <Route path="/report" component={Report} />  
-              <Route path="/contact" component={Contact} />
-              <Route path="/iotResources" component={IotResources} />
-              <Route path="/iotTechnologies" component={IotTechnologies} />
-              <Route path="/iotStatistics" component={IotStatistics} />
-              <Route path="/iotUpdates" component={IotUpdates} />
-              <Route path="/faq" component={Main} />  
-              <Route path="/forgot" component={forgot} />
-              <Route path="/login" component={Login} />  
-              <Route path="/signup" component={Signup} />  
-            </Switch>
-          </main>
-          {/* <Footer/> */}
-        </Router>
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/home" component={HomePage} />
+            <Route path="/hello" component={Helloo} />
+            <Route path="/about_us" component={aboutUs} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/news" component={News} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/report" component={Report} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/iotResources" component={IotResources} />
+            <Route path="/iotTechnologies" component={IotTechnologies} />
+            <Route path="/iotStatistics" component={IotStatistics} />
+            <Route path="/iotUpdates" component={IotUpdates} />
+            <Route path="/faq" component={Main} />
+            <Route path="/forgot" component={forgot} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }

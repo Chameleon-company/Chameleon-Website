109c43ca8a3d75042730a580d6f63c523ba078d5
Davinder-2001 <83646254+Davinder-2001@users.noreply.github.com>
2023-09-15 16:46:48 +1000

    HERO Section and Visuals for Homepage
    A HERO section has been added to the homepage with the following characteristics:
    * A prominent hero section with a captivating background image representing smart city technology.
    * The company name and tagline ("Welcome to Chameleon - Revolutionizing Urban Living Through IoT-Based Solutions").
    * A brief, engaging message that summarizes Chameleon's mission and commitment to making cities smarter, more livable, and environmentally friendly.
    * A call-to-action (CTA) button inviting visitors to "Explore Our Mission."
    
    Other visuals of the homepage are also improved.

--- front_end_project/src/pages/homepage/Homepage.js
+++ front_end_project/src/pages/homepage/Homepage.js
@@ -2,42 +2,77 @@ import "./Intro.css";
 import "./Our_Project.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import White_Logo from "./Image_homepage/White_Logo.png";
 import City from "./Image_homepage/City.png";
 import EV from "./Image_homepage/EV.jpeg";
 import Website from "./Image_homepage/Website.png";
+import Button from 'react-bootstrap/Button';
 
 function Homepage() {
   return (
     <div>
+      {/* HERO SECTION*/}
+      <div className="hero-section">
+        <div className="overlay"></div>
+        <div className="hero-content">
+          <h1>Welcome to Chameleon</h1>
+          <h2>
+            Revolutionizing Urban Living Through IoT-Based Solutions.
+          </h2>
+          <p>
+            In a rapidly evolving world, Chameleon leads the way in creating innovative solutions that transform cities into smarter, more livable, and environmentally friendly communities.
+          </p>
+          <Button color="primary" size="lg">Explore Our Mission</Button>
+        </div>
+      </div>
+
+      {/* INTRODUCTION SECTION */}
       <div>
         <div class="Introduction">
           <img
             src={White_Logo}
             alt="Chameleon logo white"
             className="img-fluid"
-            style={{ marginLeft: "30px" }}
+            style={{ marginLeft: "100px" }}
           />
           <div class="Text2">
             <p>
-              &nbsp; &nbsp; &nbsp; &nbsp; At Chameleon, out mission is to
+              At Chameleon, out mission is to
               research, create, test, document and deploy IoT-based solutions to
               enhance life through the application of smart city technologies
               including: the building of smarter cities, homes, transportation
               and energy management systems.
             </p>
-            <p style={{ marginLeft: "30px" }}>---Chameleon CEO </p>
+            <p style={{ marginLeft: "10px" }}><strong>-Chameleon CEO</strong> </p>
           </div>
         </div>
       </div>
 
+      {/* OUR PROJECTS*/}
       <div>
         <div class="Our_projects">
-          <div class="d-flex justify-content-center">
-            <h1 style={{ marginTop: "40px" }}>OUR PROJECTS</h1>
+          <div class="d-flex justify-content-center project-heading">
+            <h3 style={{ marginTop: "40px" }}>OUR PROJECTS</h3>
           </div>
 
-          <div class="d-flex justify-content-center">
+          <div className="project-container">
+            <div className="project-card">
+              <a href="#"><img src={EV} alt="EV Adoption Tools" className="project-desc" /></a>
+              <p>EV Adoption Tools</p>
+            </div>
+            <div className="project-card">
+              <a href="#"><img src={City} alt="City of Melboune Open Data Project" className="project-desc" /></a>
+              <p>City Of Melbourne Open Data</p>
+            </div>
+            <div className="project-card">
+              <a href="#"><img src={Website} alt="Website Project" className="project-desc" /></a>
+              <p>Website Uplift</p>
+            </div>
+          </div>
+
+
+          {/* <div class="d-flex justify-content-center">
             <div class="Small_rectangle">
               <a href="#">
                 <img src={EV} alt="EV Adoption Tools" class="project_picture" />
@@ -64,7 +99,7 @@ function Homepage() {
               </a>
               <p class="project_description">Website Uplift</p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>

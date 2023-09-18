eeabd2eba0b1b900366c80c9fa248c136e82c820
Vishnu <vishnu21012001@gmail.com>
2023-09-18 14:51:28 +1000

    chatbot-interface

--- front_end_project/src/App.js
+++ front_end_project/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./layouts/header/header";
 import Footer from "./layouts/footer/footer";
 import HomePage from "./pages/homepage/Homepage";
+import Chatbot from './components/Chatbot/Chatbot';
 import Helloo from "./pages/hello/hello";
 import aboutUs from "./pages/about/aboutUs";
 import Blog from "./pages/blog/blog";
@@ -19,34 +20,36 @@ import Signup from "./pages/signup/signup";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <>
-        <Router>
-          <Header />
-          <main>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/home" component={HomePage} />
-              <Route path="/hello" component={Helloo} />
-              <Route path="/about_us" component={aboutUs} />
-              <Route path="/blog" component={Blog} />
-              <Route path="/news" component={News} />
-              <Route path="/projects" component={Projects} />
-              <Route path="/report" component={Report} />  
-              <Route path="/contact" component={Contact} />
-              <Route path="/iotResources" component={IotResources} />
-              <Route path="/iotTechnologies" component={IotTechnologies} />
-              <Route path="/iotStatistics" component={IotStatistics} />
-              <Route path="/iotUpdates" component={IotUpdates} />
-              <Route path="/faq" component={Main} />  
-              <Route path="/forgot" component={forgot} />
-              <Route path="/login" component={Login} />  
-              <Route path="/signup" component={Signup} />  
-            </Switch>
-          </main>
-          {/* <Footer/> */}
-        </Router>
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/home" component={HomePage} />
+            <Route path="/hello" component={Helloo} />
+            <Route path="/about_us" component={aboutUs} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/" component={Chatbot} />
+            <Route path="/news" component={News} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/report" component={Report} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/iotResources" component={IotResources} />
+            <Route path="/iotTechnologies" component={IotTechnologies} />
+            <Route path="/iotStatistics" component={IotStatistics} />
+            <Route path="/iotUpdates" component={IotUpdates} />
+            <Route path="/faq" component={Main} />
+            <Route path="/forgot" component={forgot} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </main>
+        {/* <Footer/> */}
+      </Router>
     </>
   );
 }
---
+++ front_end_project/src/components/Chatbot/Chatbot.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Chatbot.css'; // Include your CSS here
+import Chatbox from './Chatbox';
+import Options from './options';
+import ChatInput from './ChatInput';
+
+function Chatbot() {
+    return (
+        <div className="chatbot">
+            <button className="chatbot-toggler">
+                <span className="material-symbols-outlined">more_horiz</span>
+                <span className="material-symbols-outlined">arrow_back</span>
+            </button>
+            <header>
+                <img
+                    src="images/ChameleonLogo.png"
+                    alt="Chatbot Image"
+                    className="chatbot-image"
+                />
+                <h2>Chameleon</h2>
+                <button className="options-button material-symbols-outlined">
+                    more_vert
+                </button>
+                <div className="options-container">
+                    <ul>
+                        <li>Website Project</li>
+                        <li>MOP project</li>
+                        <li>Evoleon Project</li>
+                    </ul>
+                </div>
+                <span className="close-btn material-symbols-outlined">arrow_back</span>
+            </header>
+
+            <Chatbox />
+
+            <Options />
+
+            <ChatInput />
+        </div>
+    );
+}
+
+export default Chatbot;

c27221687ed4f0eb40d2213c329d6e27f7f89ecb
FredKim98 <91114097+FredKim98@users.noreply.github.com>
2023-09-22 14:07:28 +1000

    Staff login portal update
    1. I changed the apiKey, etc. in the front_end_project/src/pages/utils/firebase.js path to what I set.
    2. I wrote the code that implements firebase under the login.js code I wrote and commented it out.

--- front_end_project/src/pages/utils/firebase.js
+++ front_end_project/src/pages/utils/firebase.js
@@ -1,70 +1,77 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 import userEvent from "@testing-library/user-event";
 
-const firebaseConfig =
-{
-    apiKey: "AIzaSyAm1ebex7fJDBas2UJs-qh8uQ3Wr6d60cU",
-    authDomain: "login-page-c4e72.firebaseapp.com",
-    projectId: "login-page-c4e72",
-    storageBucket: "login-page-c4e72.appspot.com",
-    messagingSenderId: "111235036213",
-    appId: "1:111235036213:web:c337c5f6aa95a9585cad7c",
-    // measurementId: "G-8K8JWNKKL1"
+const firebaseConfig = {
+  apiKey: "AIzaSyCgBPNTLuvfKQpZiOCJs1PRKVgy3nDhwn0",
+  authDomain: "chameleon-website-login-page.firebaseapp.com",
+  projectId: "chameleon-website-login-page",
+  storageBucket: "chameleon-website-login-page.appspot.com",
+  messagingSenderId: "462355158431",
+  appId: "1:462355158431:web:ea9cfdc4489153ca8999aa",
 };
 
 // Initialize Firebase
 const firebaseapp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt: "select_account"
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 
-signOut(auth).then(() => {
+signOut(auth)
+  .then(() => {
     console.log("logged out successfully");
-}).catch((error) => {
+  })
+  .catch((error) => {
     error.message();
-});
+  });
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
-export const createUserDocFromAuth = async (userAuth, additionalInformation = {}) => {
-    if (!userAuth.email) return;
-    const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth.email) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    const userSnapshot = await getDoc(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
 
-    if (!userSnapshot.exists()) {
-        const { fullname, email } = userAuth;
-        const createdAt = new Date();
+  if (!userSnapshot.exists()) {
+    const { fullname, email } = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await setDoc(userDocRef,
-                {
-                    fullname,
-                    email,
-                    createdAt,
-                    ...additionalInformation
-                })
-        }
-        catch (error) {
-            console.log('error', error.message)
-        }
+    try {
+      await setDoc(userDocRef, {
+        fullname,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error", error.message);
     }
+  }
 
-    return userDocRef;
-}
+  return userDocRef;
+};
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
-    return await createUserWithEmailAndPassword(auth, email, password)
-}
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 
 export const signinAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
-    return await signInWithEmailAndPassword(auth, email, password)
-}
-
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
+};

81c408a9a5fdae29b801b642392fe6ab74d4d9d2
muhammadrehanqureshi <mrq@deakin.edu.au>
2023-11-18 20:49:57 +0500

    Responsiveness of Header for all page on react.
    Import Modal from react bootstrap Cause Search Modal will visible on tablet view. Add Modal Show State , Add expand="lg" for making navbvar responsive & Change Class From navbar to navbar-dark to make menu hamburger icon color white. Changed Class basic-navbar-nav to responsive-navbar-nav to make it responsive . Added Class outer-form cause this is outer form which will hide on Tablet . Add Search Modal Button.

--- front_end_project/src/layouts/header/header.js
+++ front_end_project/src/layouts/header/header.js
@@ -2,12 +2,52 @@ import React from "react";
 import logo from "./images/Logo_No_Background.png";
 import profile from "./images/profile.jpg";
 import { Nav, Navbar, Container, Form, Button } from "react-bootstrap";
+// Import Modal from react bootstrap
+import Modal from 'react-bootstrap/Modal';
 import "bootstrap/dist/css/bootstrap.css";
+import "./header-media.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Header() {
+
+// Search Modal that will visible on tablet view.
+
+function MyVerticallyCenteredModal(props) {
   return (
-    <Navbar className="navbar bg-dark">
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Search
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      <Form className="d-flex inner-form">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="mt-2 mb-2 ml-3 me-1"
+                aria-label="Search"
+              />
+              <Button className="mt-2 mb-2 ml-3 me-1">Search</Button>
+            </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function Header() {
+  // Add Modal Show State
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    // Add expand="lg" for making navbvar responsive & Change Class From navbar to navbar-dark to make menu hamburger icon color white. 
+    <Navbar expand="lg" className="navbar-dark bg-dark">
       <Container>
         <Navbar.Brand href="/">
           <img
@@ -18,6 +58,7 @@ function Header() {
             alt="Chameleon logo"
           />
         </Navbar.Brand>
+        {/* Changed Class basic-navbar-nav to responsive-navbar-nav to make it responsive */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
@@ -48,7 +89,8 @@ function Header() {
             <Nav.Link className="text-white ml-3 mt-2" href="/login">
               LOGIN
             </Nav.Link>
-            <Form className="d-flex">
+            {/* Added Class outer-form cause this is outer form which will hide on Tablet */}
+            <Form className="d-flex outer-form">
               <Form.Control
                 type="search"
                 placeholder="Search"
@@ -57,6 +99,16 @@ function Header() {
               />
               <Button className="mt-2 mb-2 ml-3 me-1">Search</Button>
             </Form>
+
+            {/* Add Search Modal Button */}
+            <Button variant="primary" onClick={() => setModalShow(true)} className="search-btn mt-2 mb-2 ml-3 me-1 btn btn-primary">
+        Search
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
             <img
               src={profile}
               className="rounded-circle ml-3"

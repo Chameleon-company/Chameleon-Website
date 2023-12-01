1c53a8fc3752a7d6f0e0b514080e74b222835a15
hao <s223304143@deakin.edu.au>
2023-12-01 22:45:25 +1100

    Implemented Chameleon Website - Firebase Integration
    - Added Firebase integration to the Chameleon website.
    - Created 'authController.js', 'authServices.js', and 'authRoutes.js' to implement user registration, login, and password reset functionality.
    - Utilized environment variables and 'dotenv' to securely store and load the Firebase API key.
    - Ensured the security of the API key by not hardcoding it in the codebase.
    - Implemented Firebase authentication for user management.
    - Conducted extensive testing to verify the functionality of registration, login, and password reset.
    - Documented the entire process in the provided documentatio
    This commit signifies the successful integration of Chameleon Website with Firebase, enabling secure user authentication and password management.

--- back_end_project/server.js
+++ back_end_project/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const helloRoutes = require('./routes/helloRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
 app.use(helloRoutes);
+app.use("/auth",authRoutes);
 
 app.listen(3002, () => {
   console.log('Server listening on port 3002');
---
+++ back_end_project/services/authService.js
@@ -0,0 +1,47 @@
+const { initializeApp } = require('firebase/app');
+const firebaseConfig = require('../firebaseConfig');
+
+const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut } = require('firebase/auth');
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Get Firebase Authentication instance
+const auth = getAuth();
+
+// Function to create a user with email and password
+exports.createUser = async (email, password) => {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+    await sendVerificationEmail();
+    return userCredential.user;
+};
+
+// Function to sign in a user with email and password
+exports.signInUser = async (email, password) => {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+};
+
+// Function to send a verification email
+async function sendVerificationEmail() {
+    const user = auth.currentUser;
+    if (user) {
+        await sendEmailVerification(user);
+    }
+}
+
+// Function to send a password reset email
+exports.sendPasswordResetEmail = async (email) => {
+    await sendPasswordResetEmail(auth, email);
+};
+
+// Function to sign out the user
+exports.signOutUser = async () => {
+    await signOut(auth);
+};
+
+// Function to get the current user
+exports.getCurrentUser = () => {
+    return auth.currentUser;
+};

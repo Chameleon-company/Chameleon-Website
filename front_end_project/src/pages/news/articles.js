b3d965992451fef14d7b19fd4565d08d76004428
Ratanak Sovann <45008326+ratanak-sovann@users.noreply.github.com>
2023-12-08 18:30:42 +1100

    Fix infinite scroll performance issue causing the app to crash.

--- front_end_project/src/pages/news/articles.js
+++ front_end_project/src/pages/news/articles.js
@@ -36,7 +36,7 @@ const articleList = [
     {
         "key" : 5,
         "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "title" : "Melbourne Open Data heading is long", 
+        "title" : "Melbourne Open Data heading is long article 5", 
         "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
         "author" : "Jon Snow", 
         "date" : "November 27, 2023"
@@ -44,7 +44,107 @@ const articleList = [
     {
         "key" : 6,
         "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "title" : "Melbourne Open Data heading is long", 
+        "title" : "Melbourne Open Data heading is long article 6", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 7,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 7", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow",
+        "category" : "News",
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 8,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 8", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 9,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 9", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's", 
+        "author" : "Jon Snow",
+        "category" : "Insight",
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 10,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 10", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 11,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 11", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 12,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 12", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 13,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 13", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow",
+        "category" : "News",
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 14,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 14", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 15,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 15", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's", 
+        "author" : "Jon Snow",
+        "category" : "Insight",
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 16,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 16", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 17,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 17", 
+        "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
+        "author" : "Jon Snow", 
+        "date" : "November 27, 2023"
+    },
+    {
+        "key" : 18,
+        "image" : "https://images.unsplash.com/photo-1511497584788-876760111969?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "title" : "Melbourne Open Data heading is long article 18", 
         "preview" : "Melbourne Open Data refers to the City of Melbourne's initiative to make a wide range of public data sets available to the public for free.", 
         "author" : "Jon Snow", 
         "date" : "November 27, 2023"
--- front_end_project/src/pages/news/news.jsx
+++ front_end_project/src/pages/news/news.jsx
@@ -1,59 +1,83 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ArticleCard from "../../components/news/articleCard";
 import FilterBar from "../../components/news/filterBar";
 import articleList from "./articles";
 
-const debounce = (func, delay) => {
-  let timeoutId;
-  return (...args) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func(...args), delay);
+
+/**
+ * News Component
+ *
+ * This component is responsible for rendering a news and insights section on a web page.
+ * It displays a list of articles with an infinite scroll feature, allowing users to load
+ * more articles as they scroll down the page. The articles are currently sourced from a
+ * predefined list but will be fetched from a database in the future.
+ *
+ * Key Features:
+ * - Displaying news and insights.
+ * - Infinite scrolling to load more articles.
+ * - Contains a FilterBar component for searching, sorting, and filtering articles.
+ *
+ * Note: The component simulates article loading with a delay and is intended to be
+ * integrated with a backend to fetch articles from a database in a production environment.
+ */
+
+function News() {
+
+  // State variables to manage articles, loading state, and current article index
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const currentArticleIndex = useRef(0);
+
+
+  // Function to load more articles when the user scrolls
+  const loadMoreArticles = () => {
+
+    if (loading) {
+      return;
+    }
+
+    setLoading(true);
+
+    setTimeout(() => {
+      const startIndex = currentArticleIndex.current;
+      const endIndex = startIndex + 3;
+      const newArticles = articleList.slice(startIndex, endIndex);
+
+      // Append new articles to the existing ones
+      setArticles((prevArticles) => [...prevArticles, ...newArticles]); // Append more articles
+      setLoading((loading) => loading = false);
+      currentArticleIndex.current = endIndex;
+    }, 1000); // Simulated delay for loading
   };
-};
 
-class News extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: articleList,
-      loading: false,
-    };
-
-    // Debounce the handleScroll function to improve performance
-    this.debouncedHandleScroll = debounce(this.handleScroll, 200);
-    window.addEventListener("scroll", this.debouncedHandleScroll);
-  }
-
-  componentWillUnmount() {
-    // Remove the debounced event listener when the component is unmounted
-    window.removeEventListener("scroll", this.debouncedHandleScroll);
-  }
-
-  handleScroll = () => {
-    const { loading } = this.state;
-    const scrollThreshold = document.body.offsetHeight - 0.8 * window.innerHeight;
-
-    if (!loading && window.scrollY >= scrollThreshold) {
-      this.loadMoreArticles();
+   // Function to handle scrolling and trigger loading more articles
+   const handleScroll = () => {
+    if (!loading) {
+      const scrollThreshold = document.documentElement.scrollHeight - window.innerHeight;;
+      if (window.scrollY >= scrollThreshold) {
+        loadMoreArticles();
+      }
     }
   };
 
-  loadMoreArticles = () => {
-    // Simulate loading by setting loading to true
-    this.setState({ loading: true });
+  // useEffect runs when this component mounts
+  useEffect(() => {
+    
+    loadMoreArticles(); // Load the initial set of articles when the component mounts
+    window.addEventListener("scroll", handleScroll);  // Add a scroll event listener to detect when the user scrolls
 
-    setTimeout(() => {
-      // Simulating loading 5 articles at a time
-      const repeatedArticles = Array.from({ length: 5 }, () => this.state.articles).flat();
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Empty dependency array to run only on mount
 
-      this.setState((prevState) => ({
-        articles: [...prevState.articles, ...repeatedArticles],
-        loading: false,
-      }));
-    }, 20); // Simulating a delay
-  };
+  
+ 
 
-  render() {
+  
+
+
     return (
       <main className="max-w-screen-xl mx-auto">
         <section className="py-24 justify-center text-center">
@@ -67,17 +91,17 @@ class News extends Component {
             <FilterBar />
           </div>
 
-          <div className="flex flex-row flex-wrap justify-around items-baseline h-auto mb-24">
-            {this.state.articles.map((article, index) => (
+          <div className="flex flex-row flex-wrap justify-center items-baseline h-auto mb-24">
+            {articles.map((article, index) => (
               <ArticleCard key={index} {...article} />
             ))}
           </div>
 
-          {this.state.loading && <div className="spinner">Loading more articles...</div>}
+          {loading && <div className="text-center my-32">Loading articles...</div>}
+
         </section>
       </main>
     );
   }
-}
 
 export default News;

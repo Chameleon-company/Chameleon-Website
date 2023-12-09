0788fa5b6e3a6ad2149b363d977c6a523d607504
arnold-mendis <s222330109@deakin.edu.au>
2023-12-09 15:45:22 +1100

    Working Sign Up page

--- front_end_project/src/pages/signup/signup.js
+++ front_end_project/src/pages/signup/signup.js
@@ -1,197 +1,197 @@
-// --------------------------------------------
-// SIGNUP CODE BRIEF START / EXAMPLE FOR BACKEND
-// ---------------------------------------------
+import React, { Component } from "react";
+import Background from "./images/Background.png";
+import Logo from "./images/Chameleon_Logo.png";
+import Google from "./images/google.png";
+import GIT from "./images/GIT.png";
+import Microsoft from "./images/microsoft.png";
 
-// import { Container, Row } from 'react-bootstrap';
-// import React, { useState, Component } from 'react';
-// import { Outlet, Link } from 'react-router-dom'
-// import { createAuthUserWithEmailAndPassword, createUserDocFromAuth } from './utils/firebase';
-// import { useNavigate } from 'react-router-dom';
-// import heading from './images/bg.jpg'
-// import logo from './images/logo.png'
-// import goo from './images/goo.png'
-// import mic from './images/mic.png'
-// import './signup.css'
-
-// function Signup() {
-
-//     const Signup = (props) => {
-//         const navigate = useNavigate();
-
-//         const [contact, setContact] = useState({
-//             fullname: '',
-//             email: '',
-//             password: '',
-//             login: ''
-//         })
-
-//         const { fullname, email, password, login } = contact;
-
-//         const handleChange = (event) => {
-//             const { name, value } = event.target
-//             setContact((preValue) => {
-//                 return {
-//                     ...preValue,
-//                     [name]: value
-//                 }
-//             })
-//         }
-
-//         const handleSubmit = async (event) => {
-//             event.preventDefault();
-
-//             if (password !== login) {
-//                 alert('passwords not matching!')
-//                 return;
-//             }
-
-//             try {
-//                 const { user } = await createAuthUserWithEmailAndPassword(email, password);
-//                 await createUserDocFromAuth(user, { fullname });
-//                 navigate("/login")
-//             }
-//             catch (error) {
-//                 console.log('error in creating user', error)
-//             }
-//         }
-
-//         return (
-//             <Container>
-//                 <Row>
-//                     {/* <div className = "greeenBg"> */}
-//                     <div className='chamHeading'>
-//                         <img src={logo} width="100" height="100" />
-//                         <h>CHAMELEON</h>
-//                     </div>
-//                     <div className='getStarted'>
-//                         Get Started
-//                     </div>
-//                     {/* <img src ={heading} width="auto" height="auto"></img> */}
-
-//                     {/* </div> */}
-
-//                     <div className='google'>
-//                         Log in with Google
-//                         <img src={goo} width="34" height="34" />
-//                     </div>
-
-//                     <div className='microsoft'>
-//                         Log in with Microsoft
-//                         <img src={mic} width="34" height="34" />
-//                     </div>
-
-//                     <div class="fullname">
-//                         <label>Name:*</label>
-//                         <input name="Name"
-//                             onChange={handleChange}
-//                             value={contact.fullname} />
-//                     </div>
-
-//                     <div class="emailAdd">
-//                         <label>Email:*</label>
-//                         <input name="email"
-//                             onChange={handleChange}
-//                             value={contact.email} />
-//                     </div>
-
-//                     <div class="password">
-//                         <label>Password:*</label>
-//                         <input
-//                             type="password"
-//                             name="password"
-//                             onChange={handleChange}
-//                             value={contact.password} />
-//                     </div>
-
-//                     <div class="create">
-//                         <button onClick={handleSubmit}>
-//                             Login
-//                         </button>
-//                     </div>
-//                     <button class="make" hred="#home">Create Account</button>
-
-//                     {/* <div className='inputSign'>
-//                     <p class="name">NAME</p>
-
-//                     <p class="email">EMAIL ADDRESS</p>
-
-//                     <p class="pass">PASSWORD</p>
-
-//                     <p class="log">LOGIN</p>
-//                 </div> */}
-
-//                     <div className='accSign'>
-//                         <p class="ac">Have an account? Sign in</p>
-//                     </div>
-
-//                 </Row>
-//             </Container>
-//         )
-//     }
-// }
-
-
-//     export default Signup;
-
-import { Container, Row } from 'react-bootstrap';
-import React, { useState, Component } from 'react';
-import {NavLink} from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
-import { Outlet, Link } from 'react-router-dom'
-import heading from './images/bg.jpg'
-import logo from './images/logo.png'
-import goo from './images/goo.png'
-import mic from './images/mic.png'
-import './signup.css'
-
-function Signup() {
-
+class Signup extends Component {
+  render() {
     return (
-        <Container>
-            <Row>
-                {/* <div className = "greeenBg"> */}
-                <div className='chamHeading'>
-                    <img src={logo} width="100" height="100" />
-                    <h>CHAMELEON</h>
+      <div
+        style={{
+          height: "123.5vh",
+          backgroundImage: `linear-gradient(to right, transparent 43.43%, white 0%), url(${Background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "left center",
+          overflow: "hidden",
+        }}>
+        <div
+          className="container mx-auto p-0"
+          style={{ maxWidth: "80%", marginTop: "3%", border: "3px solid gray" }}>
+          <div className="grid grid-cols-12">
+            <div
+              className="md:col-span-5"
+              style={{
+                backgroundColor: "gray",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}>
+              <img
+                src={Logo}
+                alt="Logo"
+                style={{ width: "50%", height: "auto", marginBottom: "2rem" }}/>
+              <p
+                style={{
+                  fontSize: "1.7rem",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}>
+                Enhancing life through IoT-Powered Smart City Solutions
+              </p>
+              <div
+                style={{
+                  position: "absolute",
+                  right: 0,
+                  top: 0,
+                  bottom: 0,
+                  width: "1px",
+                  backgroundColor: "black",
+                }}
+              ></div>
+            </div>
+
+            <div className="md:col-span-7" style={{ padding: "8%" }}>
+              <h1
+                style={{
+                  fontWeight: "bold",
+                  marginBottom: "2rem",
+                  textAlign: "center",
+                }}>
+                Sign Up
+              </h1>
+
+              <form>
+
+              <div controlId="fName" style={{ marginBottom: "2rem" }}>
+                  <input
+                    type="text"
+                    placeholder="FIRST NAME"
+                    className="w-100"
+                    style={{
+                      backgroundColor: "#ccc",
+                      border: "1px solid black",
+                      height: "50px",
+                    }}/>
                 </div>
-                <div className='getStarted'>
-                    Get Started
-                </div>
-                {/* <img src ={heading} width="auto" height="auto"></img> */}
 
-                {/* </div> */}
-
-                <div className='google'>
-                    Log in with Google
-                    <img src={goo} width="34" height="34" />
-                </div>
-
-                <div className='microsoft'>
-                    Log in with Microsoft
-                    <img src={mic} width="34" height="34" />
+                <div controlId="lName" style={{ marginBottom: "2rem" }}>
+                  <input
+                    type="text"
+                    placeholder="LAST NAME"
+                    className="w-100"
+                    style={{
+                      backgroundColor: "#ccc",
+                      border: "1px solid black",
+                      height: "50px",
+                    }}/>
                 </div>
 
-                <div className='inputSign'>
-                   
-                    <p class="name" >NAME</p>
-
-                    <p class="email">EMAIL ADDRESS</p>
-
-                    <p class="pass">PASSWORD</p>
-
-                    <p class="log">LOGIN</p>
+                <div controlId="formEmail" style={{ marginBottom: "2rem" }}>
+                  <input
+                    type="Email"
+                    placeholder="EMAIL ADDRESS"
+                    className="w-100"
+                    style={{
+                      backgroundColor: "#ccc",
+                      border: "1px solid black",
+                      height: "50px",
+                    }}/>
                 </div>
 
-                <Nav.Link href="/login">
-                    <button className='accSign'>
-                        <p class="ac">Have an account? Sign in</p>
-                    </button>
-                </Nav.Link>
+                <div controlId="formpassword" style={{ marginBottom: "2rem" }}>
+                  <input
+                    type="password"
+                    placeholder="PASSWORD"
+                    className="w-100"
+                    style={{
+                      backgroundColor: "#ccc",
+                      border: "1px solid black",
+                      height: "50px",
+                    }}/>
+                </div>
 
-            </Row>
-        </Container>
-    )
+                <div controlId="formrepassword" style={{ marginBottom: "2rem" }}>
+                  <input
+                    type="password"
+                    placeholder="RE-PASSWORD"
+                    className="w-100"
+                    style={{
+                      backgroundColor: "#ccc",
+                      border: "1px solid black",
+                      height: "50px",
+                    }}/>
+                </div>
+
+                <p style={{ fontWeight: "bold", textAlign: "right" }}>
+                  <a href="/login">Already have an account? Sign in</a>
+                </p>
+
+                <div className="d-flex justify-content-center mb-3">
+                  <button
+                    type="submit"
+                    style={{ padding: "10px 80px", fontSize: "1rem", backgroundColor: "green", color: "white" }}>
+                    {" "}
+                    SIGNUP
+                  </button>
+                </div>
+
+                <div className="d-flex justify-content-between">
+                  <div
+                    style={{
+                      backgroundColor: "green",
+                      borderRadius: "50%",
+                      width: "40px",
+                      height: "40px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <a href="https://github.com/" target="_blank"><img src={GIT} alt="GITHub Icon" /></a>
+                  </div>
+
+                  <div
+                    style={{
+                      backgroundColor: "green",
+                      borderRadius: "50%",
+                      width: "40px",
+                      height: "40px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <a href="https://mail.google.com/" target="_blank"><img
+                      src={Google}
+                      alt="Google Icon"
+                      style={{ width: "90%", height: "60%" }}/></a>
+                  </div>
+
+                  <div
+                    style={{
+                      backgroundColor: "green",
+                      borderRadius: "50%",
+                      width: "40px",
+                      height: "40px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <a href="https://www.microsoft.com/en-us" target="_blank"><img
+                      src={Microsoft}
+                      alt="Microsoft Icon"
+                      style={{ width: "60%", height: "60%" }}/></a>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Signup;

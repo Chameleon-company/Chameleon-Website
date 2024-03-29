8c808b9fc4a31d596e52a8b5b87014556b6a5926
Umayir Feroze <umayir10@gmail.com>
2024-03-29 16:05:18 +1100

    Fix Warnings Report js

--- front_end_project/src/pages/report/report.js
+++ front_end_project/src/pages/report/report.js
@@ -1,13 +1,12 @@
-import { Container, Row, Col, ProgressBar } from 'react-bootstrap';
-import React, { Component } from 'react';
-import './report.css'
-// import cham from './images/cham.png'
-import ev from './images/ev.png'
-import mop from './images/mop.png'
-import cham from './images/cham.png'
+import { Container, Row, Col } from 'react-bootstrap';
+import React from 'react';
+import './report.css';
+import ev from './images/ev.png';
+import mop from './images/mop.png';
+import cham from './images/cham.png';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Report() {
+function Report () {
 
     return (
         <Container>
@@ -26,7 +25,7 @@ function Report() {
                         <div id="evDiv">
                             <div id="myBar">30%</div>
                             <div className='evImage'>
-                                <img src={ev} width="100" height="100" />
+                                <img alt="ev" src={ev} width="100" height="100" />
                                 <p>EV Chargers</p>
                             </div>
                         </div>
@@ -38,7 +37,7 @@ function Report() {
                         <div id="websiteDiv">
                             <div id="webBar">40%</div>
                             <div className='chamImage'>
-                                <img src={cham} width="100" height="100"></img>
+                                <img alt="cham" src={cham} width="100" height="100"></img>
                                 <p>Chameleon Website</p>
                             </div>
                         </div>
@@ -50,7 +49,7 @@ function Report() {
                         <div id="mopDiv">
                             <div id="mopBar">60%</div>
                             <div className='mopImage'>
-                                <img src={mop} width="100" height="100" />
+                                <img alt="mop" src={mop} width="100" height="100" />
                                 <p>Melbourne Open Data</p>
                             </div>
                         </div>
@@ -80,7 +79,7 @@ function Report() {
                                         Kinshuk Jain
                                     </td>
                                     <td> Janvi Gupta
-                                    <br></br>
+                                        <br></br>
                                         Michael Wei Zhang Lai
                                     </td>
                                     <td>
@@ -98,7 +97,7 @@ function Report() {
                                         Pratham
                                     </td>
                                     <td> ZAK CONSTABLE
-                                    <br></br>
+                                        <br></br>
                                         Jon Suwannakot
                                     </td>
 

d1cd73113e996c73084984fa9e337979d249db4c
Umayir Feroze <umayir10@gmail.com>
2024-05-06 19:13:56 +1000

    Fix Footer Responsiveness
    Fix footer responsiveness, and alignment across all screens.
    
    Fix dark/light mode, with icons converted to svg.

--- front_end_project/src/layouts/footer/footer.js
+++ front_end_project/src/layouts/footer/footer.js
@@ -1,20 +1,14 @@
 import chameleonLogo from "../../assets/ChameleonLogo.png";
 import deakinLogo from "./assets/Footer-Deakin.png";
-import facebookLogo from "./assets/Footer-Facebook.png";
-import twitterLogo from "./assets/Footer-Twitter.png";
-import instagramLogo from "./assets/Footer-Instagram.png";
-import emailLogo from "./assets/Footer-Email.png";
 import React, { useState, useEffect } from 'react';
-import { FaInstagram, FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram, FaFacebookSquare, FaTwitterSquare, FaSun, FaMoon } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
-// import {ChameleonLogo}  from "./ChameleonLogo.svg";
+import { colors } from '../../constants/colors';
 
 const Footer = () => {
   // Retrieve the theme from sessionStorage or default to the user's preference or 'dark'
   const storedTheme = sessionStorage.getItem('theme') || (window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
   const [theme, setTheme] = useState(storedTheme);
-
-  // Set the initial theme state
 
   // Update sessionStorage whenever the theme changes
   useEffect(() => {
@@ -26,55 +20,54 @@ const Footer = () => {
     setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
   };
 
-
   return (
     <footer className="mt-auto">
       <div className="footer-content">
-        <div className="flex flex-col-reverse md:flex-row md:justify-between">
-          <div className="flex items-center mb-4 md:mb-0 md:mr-4 justify-center">
-            <a href="/" className="mr-4">
-              <img
-                src={chameleonLogo}
-                alt="Chameleon Logo"
-                className="w-20 h-20"
-              />
+
+        {/* Responsiveness using flex box */}
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+
+          {/* Logo */}
+          <div className="p-1 d-none d-lg-flex align-self-center">
+            <a href="/" className="flex p-1">
+              <img src={chameleonLogo} alt="Chameleon Logo" className="w-10 h-10" />
             </a>
-            <div>
-              <span className="">Chameleon Company</span>
+          </div>
 
-              {/* Social Media */}
-              <div className="flex">
-                {/* Social media */}
-                <a href="/" className="mx-1">
-                  <FaInstagram size={30} />
-                </a>
-                <a href="/" className="mx-1">
-                  <FaFacebookSquare size={30} />
-                </a>
-                <a href="/" className="mx-1">
-                  <FaTwitterSquare size={30} />
-                </a>
-                <a href="/" className="mx-1">
-                  <MdOutlineEmail size={30} />
-                </a>
-              </div>
-
-              <span className=" text-xs">
-                © 2023 Copyright reserved by Chameleon
-              </span>
+          {/* Copyrights Content */}
+          <div className="p-2 mr-auto align-self-center">
+            <div className='my-auto'>
+              <span className="text-xs"> © <b>2023 Chameleon Website by</b> Chameleon <span className='d-none d-sm-inline'><b>| All Rights Reserved | Powered by</b> Deakin University</span></span>
             </div>
           </div>
-          <div className="text-center font-bold text-2xl mb-3">
-            <div>Contact us</div>
-            <div>E: chameleon@deakin.edu.au</div>
+
+          {/* Social Media */}
+          <div className="p-2 align-self-center">
+            <div className='ml-auto my-auto'>
+              <div className="flex my-auto p-auto text-decoration-none">
+                <a href="/https://www.instagram.com/deakinuniversity/?hl=en" className="mx-1">
+                  <FaInstagram color={theme === 'dark' ? colors.white : colors.black} size={30} />
+                </a>
+                <a href="https://www.facebook.com/DeakinUniversity" className="mx-1 text-decoration-none">
+                  <FaFacebookSquare color={theme === 'dark' ? colors.white : colors.black} size={30} />
+                </a>
+                <a href="/https://twitter.com/Deakin" className="mx-1 text-decoration-none">
+                  <FaTwitterSquare color={theme === 'dark' ? colors.white : colors.black} size={30} />
+                </a>
+                <a href="mailto: info@chameleon.com.au" className="mx-1 text-decoration-none">
+                  <MdOutlineEmail color={theme === 'dark' ? colors.white : colors.black} size={30} />
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center mb-2">
-            <img src={deakinLogo} alt="Deakin Logo" className="w-20 h-20" />
+
+          {/* Light-Dark Theme */}
+          <div className="p-2 align-self-center">
+            <button onClick={toggleTheme} className=" p-2 my-auto">{theme === 'dark' ? <FaMoon size={20} /> : <FaSun size={20} />}</button>
           </div>
         </div>
-        <button onClick={toggleTheme} className="mt-2 mb-2 ml-3 me-1">Theme Switch</button>
-
       </div>
+
       <style>
         {`
           .footer-content {
@@ -83,17 +76,17 @@ const Footer = () => {
               background-color: #2D6039;
               color: white;
             ` : `
-              background-color: white;
+              background-color: #d1e2c4;
               color: black;
             `}
           }
 
           .header-content {
             ${theme === 'dark' ? `
-              background-color: black;
+              background-color: #2D6039;
               color: white;
             ` : `
-              background-color: white;
+              background-color: #d1e2c4;
               color: black;
             `}
           }

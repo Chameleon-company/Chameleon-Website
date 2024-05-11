c01bd90a48d9450b44012b34f8d8832a3487e029
Umayir Feroze <umayir10@gmail.com>
2024-05-12 00:40:48 +1000

    Remove backend

--- back_end_project/controllers/helloController.js
+++
@@ -1,7 +0,0 @@
-const helloService = require('../services/helloService');
-
-exports.sayHello = (req, res) => {
-  const name = req.params.name || 'World';
-  const message = helloService.getHelloMessage(name);
-  res.send({ message });
-};

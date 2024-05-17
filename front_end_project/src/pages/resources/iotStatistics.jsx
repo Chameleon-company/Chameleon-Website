d6018d0b9207f5d86795111813eb0e876e5840ef
MatheeshaNPalliyaguruge <s222506155@deakin.edu.au>
2024-05-17 14:04:55 +1000

    modification to resource page design structure of statistics and IoT updates, resolved ssyntax errors present in signup page

--- front_end_project/src/pages/resources/iotStatistics.jsx
+++ front_end_project/src/pages/resources/iotStatistics.jsx
@@ -6,10 +6,15 @@ const content1 =
 
 class IotStatistics extends Component {
   render () {
+
+    const titleStyle = {
+      color: 'white'
+    };
+
     return (
       <Screen>
         <div className="2xl:max-w-[1320px] ml-auto mr-auto w-full sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px]">
-          <h1 className="pb-3 text-center">STATISTICS</h1>
+          <h1 className="pb-3 pt-3 text-center" style={titleStyle}>STATISTICS</h1>
         </div>
 
         <div className="mb-20 ml-auto mr-auto w-full max-w-[1000px] bg-[#D1E2C4]">
@@ -18,7 +23,7 @@ class IotStatistics extends Component {
           </h2>
           <div className="ml-3 mr-3 mt-0 flex flex-wrap">
             <div className="flex-[1_0]">
-              <p className="fw-normal fs-4 pb-3 pt-3 text-start text-black">
+              <p className="fw-normal fs-5 pb-3 pt-3 text-start text-black" style={{ textAlign: 'justify', padding: '30px'}}>
                 {content1}
               </p>
             </div>

1a04ce4247b5e3c9428951ea7802b51104569bd5
Umayir Feroze <umayir10@gmail.com>
2024-08-06 00:35:01 +1000

    Code optimisation.

--- front_end_project/src/pages/resources/iotResources.jsx
+++ front_end_project/src/pages/resources/iotResources.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useHistory } from 'react-router-dom';
 import { startTransition } from 'react';
-import Screen from '../../components/app/Screen';
 import { Card, Col, Row } from 'react-bootstrap';
+import Screen from '../../components/app/Screen';
 import './iot.css';
-
-
 
 export default function IotResources () {
   const history = useHistory();
@@ -13,6 +11,12 @@ export default function IotResources () {
   const handleCardClick = (url) => {
     startTransition(() => { history.push(url); });
   };
+
+  const contents = [
+    { id: 1, title: 'Technologies', subtitle: 'Click to view more', image: 'https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/iotTechnologies' },
+    { id: 2, title: 'Statistics', subtitle: 'Click to view more', image: 'https://images.unsplash.com/photo-1603539947678-cd3954ed515d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/iotStatistics' },
+    { id: 3, title: 'Innovation', subtitle: 'Click to view more', image: 'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/iotUpdates' }
+  ];
 
   return (
     <>
@@ -26,35 +30,17 @@ export default function IotResources () {
 
           {/* Page Content */}
           <Row className='p-5'>
-            <Col lg={4} md={6} xs={12} className='p-2'>
-              <Card className='h-100' border='rounded' onClick={() => handleCardClick('/iotTechnologies')}>
-                <Card.Img variant="top" src="https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='Technology Graphic' />
-                <Card.Body>
-                  <Card.Title>Technologies</Card.Title>
-                  <Card.Text> Click to view more </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-
-            <Col lg={4} md={6} xs={12} className='p-2'>
-              <Card className='h-100' onClick={() => handleCardClick('/iotStatistics')}>
-                <Card.Img variant="top" src="https://images.unsplash.com/photo-1603539947678-cd3954ed515d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='Technology Graphic' />
-                <Card.Body>
-                  <Card.Title>Statistics</Card.Title>
-                  <Card.Text> Click to view more </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-
-            <Col lg={4} md={6} xs={12} className='p-2' onClick={() => handleCardClick('/iotUpdates')}>
-              <Card className='h-100'>
-                <Card.Img variant="top" src="https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='Technology Graphic' />
-                <Card.Body>
-                  <Card.Title>Innovations</Card.Title>
-                  <Card.Text> Click to view more </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
+            {Array.isArray(contents) && contents.map((content) => (
+              <Col key={content.id} lg={4} md={6} xs={12} className='p-2'>
+                <Card className='h-100' border='rounded' onClick={() => handleCardClick(content.link)}>
+                  <Card.Img variant="top" src={content.image} alt='Technology Graphic' />
+                  <Card.Body>
+                    <Card.Title>{content.title}</Card.Title>
+                    <Card.Text> Click to view more </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
           </Row>
 
         </div>

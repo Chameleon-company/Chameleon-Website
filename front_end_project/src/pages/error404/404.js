9c0a63ee0463ac78a8f5d8647971416d48c53eeb
Stephhl <139114003+Stephhl@users.noreply.github.com>
2024-08-08 21:12:41 +1000

    Added navigation bar and footer to 404

--- front_end_project/src/pages/error404/404.js
+++ front_end_project/src/pages/error404/404.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import Screen from "../../components/app/Screen";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-black text-white">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold mb-4">404</h1>
-        <h2 className="text-3xl font-semibold mb-2">Page Not Found</h2>
-        <p className="text-lg">
-          Oops! The page you're looking for doesn't exist.
-        </p>
-        <p className="mt-4">
-          You may have mistyped the URL or the page has been moved.
-        </p>
-        <a href="/" className="mt-6 text-blue-500 hover:underline">Go back to home</a>
+    <Screen>
+      <div className="flex justify-center items-center min-h-screen bg-black text-white">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-4">404</h1>
+          <h2 className="text-3xl font-semibold mb-2">Page Not Found</h2>
+          <p className="text-lg">
+            Oops! The page you're looking for doesn't exist.
+          </p>
+          <p className="mt-4">
+            You may have mistyped the URL or the page has been moved.
+          </p>
+          <a href="/" className="mt-6 text-blue-500 hover:underline">Go back to home</a>
+        </div>
       </div>
-    </div>
+    </Screen>
   );
 };
 

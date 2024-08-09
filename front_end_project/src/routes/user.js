12694ae3ade1a2304245e5483aeea83e1c688681
MJahanzaibKhan <programmerjahanzaib7@gmail.com>
2024-08-09 17:39:41 +1000

    working on change password functionality

--- front_end_project/src/routes/user.js
+++ front_end_project/src/routes/user.js
@@ -13,3 +13,22 @@ export const signOut = async () => {
     }
 };
 
+export const changePassword = async (currentPassword, newPassword) => {
+    try {
+        const response = await axios.post('http://localhost:3002/auth/change-password', {
+            currentPassword,
+            newPassword
+        });
+
+        if (response.data.success) {
+            console.log('Password has changed successfully');
+            return { success: true };
+        } else {
+            return { success: false, message: response.data.message || 'Password change failed.' };
+        }
+    } catch (error) {
+        console.error('Error changing password:', error);
+        return { success: false, message: error.message || 'An unknown error occurred.' };
+    }
+};
+

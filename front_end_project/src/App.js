5f366f48fd733694b27e77ceaac884b5cdb5880b
Umayir Feroze <umayir10@gmail.com>
2024-09-03 02:29:04 +1000

    Code clean up

--- front_end_project/src/App.js
+++ front_end_project/src/App.js
@@ -1,6 +1,5 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import Screen from './components/app/Screen';
 import NotificationComponent from './components/notificationComp/NotificationComponent';
 import NotFoundPage from './pages/error404/404.js';
 import Profile from './pages/profile/profile.js';
@@ -16,16 +15,9 @@ const IotResources = lazy(() => import('./pages/resources/iotResources'));
 const IotTechnologies = lazy(() => import('./pages/resources/iotTechnologies'));
 const IotStatistics = lazy(() => import('./pages/resources/iotStatistics'));
 const IotUpdates = lazy(() => import('./pages/resources/iotUpdates'));
-const Main = lazy(() => import('./pages/faq/main'));
-const Report = lazy(() => import('./pages/report/report'));
 const Login = lazy(() => import('./pages/login/login'));
-const Logout = lazy(() => import('./pages/logout/logout'));
 const Signup = lazy(() => import('./pages/signup/signup'));
 const ResetPassword = lazy(() => import('./pages/reset_password/reset_demo'));
-const newsSignupForm = lazy(() => import('./pages/email_newsletter_forms/subscribe'),);
-const newsUnsubForm = lazy(() => import('./pages/email_newsletter_forms/unsubscribe'),);
-const newsRemoveForm = lazy(() => import('./pages/email_newsletter_forms/remove'),);
-const SearchResults = lazy(() => import('./pages/search/SearchResults'));
 const Support = lazy(() => import('./pages/support/support'));
 
 
@@ -43,29 +35,22 @@ function App () {
           <main>
             <Switch>
               <Route path="/project/:projectId" component={ProjectDetails} />
-              <Route exact path="/" component={Screen} />
+              <Route exact path="/home" component={HomePage} />
               <Route exact path="/home" component={HomePage} />
               <Route path="/aboutus" component={aboutUs} />
               <Route path="/news" component={News} />
               <Route path="/chatbot" component={Chatbot} />
               <Route path="/projects" component={Projects} />
-              <Route path="/report" component={Report} />
               <Route path="/iotResources" component={IotResources} />
               <Route path="/iotTechnologies" component={IotTechnologies} />
               <Route path="/iotStatistics" component={IotStatistics} />
               <Route path="/iotUpdates" component={IotUpdates} />
               <Route path="/profile" component={Profile} />
               <Route path="/posts/:id" component={Post} />
-              <Route path="/faq" component={Main} />
               <Route path="/login" component={Login} />
-              <Route exact path="/logout" component={Logout} />
               <Route path="/signup" component={Signup} />
               <Route path="/reset" component={ResetPassword} />
               <Route path="/support" component={Support} />
-              <Route path="/newsSignupForm" component={newsSignupForm} />
-              <Route path="/newsUnsubForm" component={newsUnsubForm} />
-              <Route path="/newsRemoveForm" component={newsRemoveForm} />
-              <Route path="/search-results" component={SearchResults} />
               {/* {session ? (<Route path='/' component={HomePage}/>) : (<Route path='/' component={Screen}/>)} */}
               <Route component={NotFoundPage} />
               {/* {rememberMe ? (<Route path='/' component={HomePage}/>) : (session ? (<Route path='/' component={HomePage}/>) : (<Route path='/' component={Screen}/>))} */}

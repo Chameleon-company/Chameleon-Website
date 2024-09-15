16f4e01874a8f30e4cb2e724971bce84131ab894
Stephhl <139114003+Stephhl@users.noreply.github.com>
2024-09-15 19:31:03 +1000

    Replaced social media images with react icons

--- front_end_project/src/pages/login/login.js
+++ front_end_project/src/pages/login/login.js
@@ -2,9 +2,7 @@
 import React, { Component } from "react";
 import "./login.css"; // Make sure the path is correct
 import Logo from "./image/Chameleon_Logo.png";
-import Google from "./image/google.png";
-import Linkedin from "./image/linkedin.png";
-import Microsoft from "./image/microsoft.png";
+import { FaGoogle, FaLinkedin, FaMicrosoft } from 'react-icons/fa';
 import Screen from "../../components/app/Screen";
 import { Redirect } from "react-router-dom";
 import { auth } from "../utils/firebase";
@@ -264,9 +262,9 @@ class Login extends Component {
                       </form>
                       <p className="text-center mt-3">-- or login with --</p>
                       <div className="flex justify-center items-center space-x-4">
-                        <a href=""><img className="inline h-9 w-9" src={Google}></img></a>
-                        <a href=""><img className="inline h-12 w-12" src={Linkedin}></img></a>
-                        <a href=""><img className="inline h-7 w-7" src={Microsoft}></img></a>
+                        <a href="#"><FaGoogle className="h-9 w-9" /></a>
+                        <a href="#"><FaLinkedin className="h-9 w-9" /></a>
+                        <a href="#"><FaMicrosoft className="h-9 w-9" /></a>
                       </div>
                       <p className="text-center pt-4">
                         Don't have an account?{" "}
